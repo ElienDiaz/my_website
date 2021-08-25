f5210ea04635675ab9005512e6b793bec16f8dc6
Elie <ediaz30@gatech.edu>
2021-08-24 22:18:10 -0600

    Most of the home page is done!

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,17 +1,44 @@
-<section>
-    <div class="block">
-        <h1> Welcome to my page! (hopefully I can figure out a top margin here so that this is centered and bigger)</h1>
+<section class="section">
+    <div class="block has-text-centered is-size-3">
+        <h1>Welcome to my page!</h1>
     </div>
-    <div class="block">
-        (Placeholder until I can figure out how to put a picture here)
+    <div class="block is-flex is-horizontal-center">
+        <figure class="image is-128x128">
+            <img
+                class="is-rounded"
+                src="./melodica.jpg"
+                alt="This is supposed to be a silly picture of me playing melodica"
+            />
+        </figure>
     </div>
-    <div class="block">
-        Hi! My name is Elie Diaz and this paragraph is meant to be a quick intro to me .
+    <div class="block is-fullhd">
+        Hi! My name is Elie Diaz and this paragraph is meant to be a quick intro
+        to me .
     </div>
-    <div class="block">
-        This paragraph is supposed to just be a quick description of my site so you can navigate it
-    </div>
-    <div class="block">
-        This paragraph should <strong>be centered</strong> for links and things to contact me
+    <div class="block is-fullhd">
+        This paragraph is supposed to just be a quick description of my site so
+        you can navigate it
     </div>
 </section>
+<section class="section">
+    <div class="block has-text-centered is-size-5">Contact Me!</div>
+    <div class="block has-text-centered">
+        <p>Email me at
+            <a href="mailto:eliericardo@hotmail.com">eliericardo@hotmail.com</a>
+            <br />
+            Find me on
+            <a href="www.linkedin.com/in/eliediaz">LinkedIn</a>
+            <br />
+            Check out my
+            <a href="https://github.com/eliendiaz">GitHub</a>
+            <br />
+            And my
+            <a href="https://www.instagram.com/charliethebuzz/">Travel Instagram!</a></p>
+    </div>
+</section>
+
+<style>
+    .is-horizontal-center {
+        justify-content: center;
+    }
+</style>
