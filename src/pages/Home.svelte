a59e93edb80c1fb4b7d20bd0661b69ebb9996e65
Elie <ediaz30@gatech.edu>
2021-08-24 22:56:34 -0600

    Home Page is mostly finished now

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -11,19 +11,58 @@
             />
         </figure>
     </div>
-    <div class="block is-fullhd">
-        Hi! My name is Elie Diaz and this paragraph is meant to be a quick intro
-        to me .
+    <div class="columns">
+        <div class="column">
+            <div class="block has-text-centered is-size-5">
+                <h1>About Me!</h1>
+            </div>
+            <div class="block has-text-justified is-size-7">
+                Hi! My name is Elie Diaz. I'm currently a PhD student at the
+                University of Utah, working on Computer Graphics. My first two
+                degrees (BS and MS) were from Georgia Tech in Computer Science,
+                so I have been learning more about the field for a few years
+                now. As far as my projects are concerned, I have done a decent
+                amount of work in simulation. Go ahead and explore my site to
+                learn more about me!
+            </div>
+            <!-- <div class="block is-size-7">
+                I've also built up experience teaching! During my undergrad and
+                master's, I was a TA for Georgia Tech's undergrad Computer
+                Graphics course (CS 3451), under various faculty at Tech. After
+                about 4 years of being a TA, I was awarded the College of
+                Computing's "Outstanding Undergraduate TA" Award.
+            </div> -->
+            <!-- <div class="block is-size-7">
+                My other projects are especially focused on music, as you can
+                see on the other tabs. Throughout my years in ensembles, I've
+                built up a music resume that includes performances with Piano
+                and Voice as well as experience directing and arranging music
+                for small ensembles. I'm an especially big fan of Barbershop
+                Quartet Singing, having been a member of the Barbershop Harmony
+                Society on and off since high school.
+            </div> -->
+        </div>
+        <div class="column">
+            <div class="block has-text-centered is-size-5">
+                <h1>About My Website!</h1>
+            </div>
+            <div class="block has-text-justified is-size-7">
+                I built this site using Svelte and Bulma as frameworks. With
+                these, I was able to split the site into several tabs. You'll
+                find that project tab useful if you're interested in finding a
+                general overlook of the Computer Graphics projects I've worked
+                on and am working on. Another large part of my life revolves
+                around music, so you can go to that tab to check out the various
+                music-related projects I'm able to share. Finally, that last CV tab
+                should take you to a link to download a pdf of my most recent
+                version of my CV.
+            </div>
+        </div>
     </div>
-    <div class="block is-fullhd">
-        This paragraph is supposed to just be a quick description of my site so
-        you can navigate it
-    </div>
-</section>
-<section class="section">
     <div class="block has-text-centered is-size-5">Contact Me!</div>
     <div class="block has-text-centered">
-        <p>Email me at
+        <p>
+            Email me at
             <a href="mailto:eliericardo@hotmail.com">eliericardo@hotmail.com</a>
             <br />
             Find me on
@@ -33,8 +72,14 @@
             <a href="https://github.com/eliendiaz">GitHub</a>
             <br />
             And my
-            <a href="https://www.instagram.com/charliethebuzz/">Travel Instagram!</a></p>
+            <a href="https://www.instagram.com/charliethebuzz/"
+                >Travel Instagram!</a
+            >
+        </p>
     </div>
+</section>
+<section class="section">
+    
 </section>
 
 <style>
