23827d7596f85fa4958d7f54608c727c3c91bb9a
Elie <ediaz30@gatech.edu>
2021-08-24 21:05:17 -0600

    Auto-Formatted Stuff before continuing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
   import { onDestroy } from "svelte";
   let menuOpen = false;
-//   import { resumeRoute, route } from "../route";
-//   import Link from "../components/Link.svelte";
-  
-//   const unsubscribe = route.subscribe(() => {
-//     menuOpen = false;
-//   });
-//   onDestroy(unsubscribe);
+  //   import { resumeRoute, route } from "../route";
+  //   import Link from "../components/Link.svelte";
+
+  //   const unsubscribe = route.subscribe(() => {
+  //     menuOpen = false;
+  //   });
+  //   onDestroy(unsubscribe);
 </script>
+
+<!-- <nav class="page-nav"> -->
+<nav class="navbar">
+  <div class="navbar-brand">
+    <div class="navbar-item">Elie Diaz</div>
+    <a
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarMenu"
+    >
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </a>
+  </div>
+  <div id="navbarMenu" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item"> Home </a>
+      <a class="navbar-item"> Projects </a>
+      <a class="navbar-item"> Music </a>
+      <a class="navbar-item"> CV </a>
+    </div>
+    <div class="navbar-end" />
+  </div>
+</nav>
 
 <style>
   .navbar {
@@ -20,15 +47,14 @@
       flex-grow: 1;
       flex-shrink: 0;
     } */
-  
-  
+
   .navbar-burger {
-    color: #a8d1ce
+    color: #a8d1ce;
   }
 
   .navbar-item {
-    font-family: 'Fira Sans';
-	  color: #a8d1ce;
+    font-family: "Fira Sans";
+    color: #a8d1ce;
     /* background:hsl(0, 100%, 50%) */
   }
   /* ul.mobile-menu {
@@ -50,36 +76,3 @@
     background: rgba(0, 0, 0, 0.25);
   } */
 </style>
-
-<!-- <nav class="page-nav"> -->
-  <nav class="navbar">
-    
-    <div class="navbar-brand">
-      <div class="navbar-item">
-        Elie Diaz
-      </div>
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div id="navbarMenu" class="navbar-menu">
-      <div class="navbar-start">
-        <a class="navbar-item" >
-          Home
-        </a>
-        <a class="navbar-item">
-          Projects
-        </a>
-        <a class="navbar-item">
-          Music
-        </a>
-        <a class="navbar-item">
-          CV
-        </a>
-      </div>
-      <div class="navbar-end">
-      </div>
-    </div>
-  </nav>
